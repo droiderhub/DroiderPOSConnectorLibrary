d19cf30fc1b12ff98f76490f3c221f24ebda5ed1
Satyajit Tarafdar <sajittarafdar@gmail.com>
2023-04-01 19:11:54 +0530

    all test commit

---
+++ app/src/main/java/com/droider/droidersdk/MainActivity.java
@@ -0,0 +1,34 @@
+package com.droider.droidersdk;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.droider.droiderposconnectorlibrary.TheDroider;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button send_amount;
+    TextView result_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        send_amount = findViewById(R.id.send_amount);
+        result_tv = findViewById(R.id.result_tv);
+        result_tv.setText(getIntent().getStringExtra("DROIDER_AMOUNT_RESULT"));
+        Log.d("DROIDER_APPLICATION", getApplicationContext().getPackageName());
+        Log.d("DROIDER_APPL_name", this.getClass().getSimpleName());
+
+        send_amount.setOnClickListener(v -> {
+//            DroiderManager.getInstance().sendAmount(getApplicationContext(), getIntent());
+            TheDroider.connectPOS().sendAmount(getApplicationContext(), getIntent());
+        });
+    }
+
+}
